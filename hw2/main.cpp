9c52f49642aef3037aedfa8ac1e7fb0fcda50222
Azamat Sabyrbayev <azamat.sabyrbayev@btsdigital.kz>
2021-10-12 17:11:21 +0600

    Format code

--- hw2/main.cpp
+++ hw2/main.cpp
@@ -1,34 +1,34 @@
 #include <iostream>
+
 #include "matmul.h"
 
 using namespace std;
 
 const int N_SIZE = 2048;
 
-int main()
-{
-    double* A = new double[N_SIZE * N_SIZE];
-    double* B = new double[N_SIZE * N_SIZE];
-    double* C = new double[N_SIZE * N_SIZE];
-    double* V = new double[N_SIZE];
-    double* D = new double[N_SIZE];
+int main() {
+  double * A = new double[N_SIZE * N_SIZE];
+  double * B = new double[N_SIZE * N_SIZE];
+  double * C = new double[N_SIZE * N_SIZE];
+  double * V = new double[N_SIZE];
+  double * D = new double[N_SIZE];
 
-    RandomMatrix(A, N_SIZE);
-    RandomMatrix(B, N_SIZE);
-    RandomVector(V, N_SIZE);
+  RandomMatrix(A, N_SIZE);
+  RandomMatrix(B, N_SIZE);
+  RandomVector(V, N_SIZE);
 
-    cout << "N = " << N_SIZE << endl;
+  cout << "N = " << N_SIZE << endl;
 
-    double rtime1 = CalcMatMulTime_ijk(A, B, C, N_SIZE);
-    cout << "Matrix mult: ijk run time is: " << rtime1 << endl;
+  double rtime1 = CalcMatMulTime_ijk(A, B, C, N_SIZE);
+  cout << "Matrix mult: ijk run time is: " << rtime1 << endl;
 
-    double rtime2 = CalcMatMulTime_jik(A, B, C, N_SIZE);
-    cout << "Matrix mult: jik run time is: " << rtime2 << endl;
+  double rtime2 = CalcMatMulTime_jik(A, B, C, N_SIZE);
+  cout << "Matrix mult: jik run time is: " << rtime2 << endl;
 
-    double rtime3 = CalcMatMulTime_kij(A, B, C, N_SIZE);
-    cout << "Matrix mult: kij run time is: " << rtime3 << endl;
+  double rtime3 = CalcMatMulTime_kij(A, B, C, N_SIZE);
+  cout << "Matrix mult: kij run time is: " << rtime3 << endl;
 
-    double rtime4 = CalcMatVecMulTime(A, V, D, N_SIZE);
-    cout << "Matrix to vector mult: run time is: " << rtime4 << endl;
-    return 0;
+  double rtime4 = CalcMatVecMulTime(A, V, D, N_SIZE);
+  cout << "Matrix to vector mult: run time is: " << rtime4 << endl;
+  return 0;
 }
--- hw2/matmul.cpp
+++ hw2/matmul.cpp
@@ -1,116 +1,96 @@
 #include <time.h>
+
 #include <cstdlib>
 
 #include <iostream>
 
 using namespace std;
 
-
-void ZeroMatrix(double *A, size_t N)
-{
-    for (size_t i = 0; i < N; i++)
-    {
-        for (size_t j = 0; j < N; j++)
-        {
-            A[i * N + j] = 0.0;
-        }
+void ZeroMatrix(double * A, size_t N) {
+  for (size_t i = 0; i < N; i++) {
+    for (size_t j = 0; j < N; j++) {
+      A[i * N + j] = 0.0;
     }
+  }
 }
 
-void ZeroMatrix(double *A, size_t R, size_t C)
-{
-    for (size_t i = 0; i < C; i++)
-    {
-        for (size_t j = 0; j < R; j++)
-        {
-            A[i * R + j] = 0.0;
-        }
+void ZeroMatrix(double * A, size_t R, size_t C) {
+  for (size_t i = 0; i < C; i++) {
+    for (size_t j = 0; j < R; j++) {
+      A[i * R + j] = 0.0;
     }
+  }
 }
 
-void RandomMatrix(double *A, size_t N)
-{   
-    srand(time(NULL));
-
-    for (int i = 0; i < N; i++)
-    {
-        for (int j = 0; j < N; j++)
-        {
-            A[i * N + j] = rand() / RAND_MAX;
-        }
+void RandomMatrix(double * A, size_t N) {
+  srand(time(NULL));
+  for (int i = 0; i < N; i++) {
+    for (int j = 0; j < N; j++) {
+      A[i * N + j] = rand() / RAND_MAX;
     }
+  }
 }
 
-void RandomVector(double *A, size_t N)
-{
-    srand(time(NULL));
+void RandomVector(double * A, size_t N) {
+  srand(time(NULL));
+  for (int i = 0; i < N; i++) {
+    A[i] = rand() / RAND_MAX;
+  }
+}
 
-    for (int i = 0; i < N; i++)
-    {
-        A[i] = rand() / RAND_MAX;
+double CalcMatMulTime_ijk(double * A, double * B, double * C, size_t N) {
+  clock_t tStart = clock();
+  size_t i, j, k;
+
+  ZeroMatrix(C, N);
+
+  for (i = 0; i < N; i++)
+    for (j = 0; j < N; j++) {
+      for (k = 0; k < N; k++)
+        C[i * N + j] = C[i * N + j] + A[i * N + k] * B[k * N + j];
     }
+  double rtime = (double)(clock() - tStart) / CLOCKS_PER_SEC;
+  return rtime;
 }
 
-double CalcMatMulTime_ijk(double *A, double *B, double *C, size_t N)
-{
-    clock_t tStart = clock();
-    size_t i, j, k;
-    
-    ZeroMatrix(C, N);
+double CalcMatMulTime_jik(double * A, double * B, double * C, size_t N) {
+  clock_t tStart = clock();
+  size_t i, j, k;
 
-    for (i = 0; i < N; i++)
-        for (j = 0; j < N; j++)
-        {
-            for (k = 0; k < N; k++)
-                C[i * N + j] = C[i * N + j] + A[i * N + k] * B[k * N + j];
-        }
-    double rtime = (double)(clock() - tStart) / CLOCKS_PER_SEC;
-    return rtime;
+  ZeroMatrix(C, N);
+
+  for (j = 0; j < N; j++)
+    for (i = 0; i < N; i++) {
+      for (k = 0; k < N; k++)
+        C[i * N + j] = C[i * N + j] + A[i * N + k] * B[k * N + j];
+    }
+  double rtime = (double)(clock() - tStart) / CLOCKS_PER_SEC;
+  return rtime;
 }
 
-double CalcMatMulTime_jik(double *A, double *B, double *C, size_t N)
-{
-    clock_t tStart = clock();
-    size_t i, j, k;
+double CalcMatMulTime_kij(double * A, double * B, double * C, size_t N) {
+  clock_t tStart = clock();
+  size_t i, j, k;
 
-    ZeroMatrix(C, N);
+  ZeroMatrix(C, N);
 
+  for (k = 0; k < N; k++)
+    for (i = 0; i < N; i++) {
+      for (j = 0; j < N; j++)
+        C[i * N + j] = C[i * N + j] + A[i * N + k] * B[k * N + j];
+    }
+  double rtime = (double)(clock() - tStart) / CLOCKS_PER_SEC;
+  return rtime;
+}
+
+double CalcMatVecMulTime(double * A, double * B, double * C, size_t N) {
+  clock_t tStart = clock();
+  size_t i, j;
+  ZeroMatrix(C, N, 1);
+
+  for (i = 0; i < N; i++)
     for (j = 0; j < N; j++)
-        for (i = 0; i < N; i++)
-        {
-            for (k = 0; k < N; k++)
-                C[i * N + j] = C[i * N + j] + A[i * N + k] * B[k * N + j];
-        }
-    double rtime = (double)(clock() - tStart) / CLOCKS_PER_SEC;
-    return rtime;
-}
-
-double CalcMatMulTime_kij(double *A, double *B, double *C, size_t N)
-{
-    clock_t tStart = clock();
-    size_t i, j, k;
-
-    ZeroMatrix(C, N);
-
-    for (k = 0; k < N; k++)
-        for (i = 0; i < N; i++)
-        {
-            for (j = 0; j < N; j++)
-                C[i * N + j] = C[i * N + j] + A[i * N + k] * B[k * N + j];
-        }
-    double rtime = (double)(clock() - tStart) / CLOCKS_PER_SEC;
-    return rtime;
-}
-
-double CalcMatVecMulTime(double *A, double *B, double *C, size_t N)
-{
-    clock_t tStart = clock();
-    size_t i, j;
-    ZeroMatrix(C, N, 1);
-    
-    for (i = 0; i < N; i++)
-        for (j = 0; j < N; j++)
-            C[i] = C[i] + A[i * N + j] * B[j];
-    double rtime = (double)(clock() - tStart) / CLOCKS_PER_SEC;
-    return rtime;
+      C[i] = C[i] + A[i * N + j] * B[j];
+  double rtime = (double)(clock() - tStart) / CLOCKS_PER_SEC;
+  return rtime;
 }
