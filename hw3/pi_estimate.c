15c419ceceec563151feb3f1824080519449c7b9
Azamat Sabyrbayev <azamat.sabyrbayev@btsdigital.kz>
2021-10-22 00:53:50 +0600

    HW3 done

---
+++ hw3/pi_estimate.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include <time.h>
+#include <omp.h>
+
+
+int main() {
+    int i, count, tid;
+    double x, y, z, pi;
+    unsigned long n = 1000000;
+    unsigned int seed;
+    count = 0;
+
+#pragma omp parallel private(x, y, z, tid, seed) reduction(+:count)
+    tid = omp_get_thread_num();
+    seed = (unsigned) time(NULL);
+    seed = 1;
+    seed = (seed & 0xFFFFFFF0) | (tid + 1);
+    srand(seed);
+
+#pragma omp for 
+    for(i = 0; i < n; ++i) {
+        x = ((double)rand_r(&seed) / RAND_MAX) * 2 - 1;
+        y = ((double)rand_r(&seed) / RAND_MAX) * 2 - 1;
+        z = x * x + y * y;
+        if(z <= 1)
+            count += 1;
+    }
+#pragma omp barrier
+    pi = (double) count / n * 4;
+    printf("Approximate PI = %f\n", pi);
+    return(0);
+}
